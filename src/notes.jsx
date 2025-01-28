335ce183354268054842b44c7e990c79eaa0a6b0
type sh,t <olzhas.dossaibek.dev@gmail.com>
2025-01-28 10:19:56 +0500

    🛠️💡282. Setting Up a New Project: ‘Fast React Pizza Co.’
    - step 1 - create react app via vite
    - step 2 - installs all packages - installs eslint - creates and sets up eslint file - sets up vite.config file
    - step 3 - deletes unnecessary files - launches the app - shows that eslint works (gives warnings)
    
    - in this lecture: set up the project
    - next: plan the app from complete scratch, no starter files! Exciting!
    
    Desired behavior:
    - Set up the starter code for the project
    
    Underlying implementation:
    - Same as above + bug in Q&A

---
+++ src/notes.jsx
@@ -0,0 +1,62 @@
+// Section 22: React Router With Data Loading (v6.4+) [281-292]
+// --------------------------------------------------
+
+// 1/4. Set up project
+// 💡 279. Introduction to Part 4
+// 💡 281. Section Overview
+// 🛠️ 282. Setting Up a New Project: "Fast React Pizza Co."
+// 💡283. Application Planning
+// 💡284. Setting Up a Professional File Structure
+
+// 2/4. Set up React Router
+// 💡285. A New Way Of Implementing Routes
+// 💡286. Building the App Layout
+
+// 3/4. Fetch data + error handling
+// 💡287. Fetching Data With React Router "Loaders": Pizza Menu
+// 💡288. Displaying a Loading Indicator
+// 💡289. Handling Errors With Error Elements
+// 💡290. Fetching Orders
+
+// 4/4. Write data + error handling
+// 💡291. Writing Data With React Router "Actions"
+// 💡292. Error Handling in Form Actions
+
+// --------------------------------------------------
+// --------------------------------------------------
+
+// Section 23: [Optional] Tailwind CSS Crash Course: Styling the App
+// --------------------------------------------------
+
+// 1/3. Tailwind setup + basics: 293-303
+// 💡293. Section Overview
+// 💡294. What is Tailwind CSS?
+// 💡295. Setting Up Tailwind CSS
+// 💡296. Working With Color
+// 💡297. Styling Text
+// 💡298. The Box Model: Spacing, Borders, and Display
+// 💡299. Responsive Design
+// 💡300. Using Flexbox
+// 💡301. Using CSS Grid
+// 💡302. Styling Buttons: Element States and Transitions
+// 💡303. Styling Form Elements
+
+// 2/3. Intermediate + Advanced topics: 304-307
+// 💡304. Reusing Styles With @apply
+// 💡305. Reusing Styles With React Components
+// 💡306. Absolute Positioning, z-index, and More
+// 💡307. Configuring Tailwind: Custom Font Family
+
+// 3/3. Style Components: 308-311
+// 💡308. Styling the Menu
+// 💡309. Styling the Cart
+// 💡310. Styling the Order Form
+// 💡311. Styling the Order Overview
+
+// --------------------------------------------------
+// --------------------------------------------------
+
+// Section 24: Adding Redux and Advanced React Router
+// --------------------------------------------------
+
+// 💡312. Section Overview
