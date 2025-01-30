76d485bd2267e859d21476a59808cad9d8ba6814
type sh,t <olzhas.dossaibek.dev@gmail.com>
2025-01-30 21:04:37 +0500

    🛠️💡285. A New Way Of Implementing Routes
    - step 1 - install React Router - open documentations - v.6 benefits: load data and submit data using forms
    - step 2 - router works (RouterProvider, createBrowserRouter + array, imports - compares it with the previous approach of using React Router
    - step 3 - creates and links all routes (/, /menu, /cart, /order/new, /order/:orderId) - skips * route because in v.6 this is done in a new way
    
    - in this lecture: React Router and new way of implementing Routes
    - next: no comment from Jonas
    
    Desired behavior:
    0. Set up React Router v. 6.4
    1. Create Routes and link Pages to them
    
    Underlying implementation:
    1. RouterProvider + router Prop, createBrowserRouter() + array for 5 paths

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -36,7 +36,8 @@ function CreateOrder() {
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Let&apos;s go!</h2>
+      {/* <h2>Ready to order? Let's go!</h2> */}
 
       <form>
         <div>
