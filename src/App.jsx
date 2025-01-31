9a77dbb8496a80c67019e075bff87d2326097b8c
type sh,t <olzhas.dossaibek.dev@gmail.com>
2025-01-31 22:02:19 +0500

    🛠️💡287. Fetching Data With React Router Loaders: Pizza Menu
    - step 1 - successfully connected loader function on the /menu Route - loader, useLoaderData() - render-as-you-fetch strategy (get both page and data)
    - step 2 - improve UI (Menu.jsx, .map() + Item Component)
    
    - in this lecture: learn about React Router's new data loading feature called Loaders
    - next: display loading indicator
    
    Desired behavior:
    - Show data from backend (menu items) in UI
    
    Underlying implementation:
    - Create and export loader in Menu.jsx with getMenu(), update ‘/menu’ Route with imported loader in App.jsx, read the loader in Page using useLoaderData() in Menu.jsx
    - .map(), MenuItem + pizza Prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -15,7 +15,11 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
-      { path: "/menu", element: <Menu /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
       {
         path: "/cart",
         element: <Cart />,
