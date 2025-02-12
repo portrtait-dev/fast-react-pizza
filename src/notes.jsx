cb6132a5dfbd9e81402e360aff7b49cb35380c9c
type sh,t <olzhas.dossaibek.dev@gmail.com>
2025-02-12 10:15:28 +0500

    🛠️💡300. Using Flexbox
    - step 1 - flex, items-center, justify-between for CartOverview.jsx
    - step 2 - same as previous for Header.jsx - show Username on larger screens
    - step 3 - add responsive classes to text and padding for Home.jsx
    
    - in this lecture: how to implement flexbox in Tailwind
    - next: CSS grid
    
    Desired behavior:
    - Show how to style the flexbox with Tailwind
    
    Underlying implementation:
    - Add flex in Header.jsx
    - Add flex in CartOverview.jsx
    - Add responsive font size and padding in Home.jsx
    - Add responsive border box in Username.jsx

--- src/notes.jsx
+++ src/notes.jsx
@@ -36,7 +36,7 @@
 // 🛠️💡297. Styling Text
 // 🛠️💡298. The Box Model: Spacing, Borders, and Display
 // 🛠️💡299. Responsive Design
-// 💡300. Using Flexbox
+// 🛠️💡300. Using Flexbox
 // 💡301. Using CSS Grid
 // 💡302. Styling Buttons: Element States and Transitions
 // 💡303. Styling Form Elements
